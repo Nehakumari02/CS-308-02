0613d40fe579a4c7f0c82fb8fdb34232da0f39a5
cs-308 <nehapanwal02@gmail.com>
2022-08-29 17:11:35 +0530

    2nd commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CS-308-02
+the program take a value and find the factorial of that if the value is negative than it ask to re-enter the number
--- factorial.cpp
+++ factorial.cpp
@@ -3,6 +3,7 @@
 int factorial(int n){
     if(n<0){
         cout<<"re-enter a positive number";
+        cout<<"hello";
         return 0;
     }
     else if(n!=1){
