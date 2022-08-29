9feb2d9195c03ce0364c0de1509a1878a60212ba
cs-308 <nehapanwal02@gmail.com>
2022-08-29 16:27:16 +0530

    1st commit

--- factorial.cpp
+++ factorial.cpp
@@ -1,7 +1,11 @@
 #include "functions.h"
 
 int factorial(int n){
-    if(n!=1){
+    if(n<0){
+        cout<<"re-enter a positive number";
+        return 0;
+    }
+    else if(n!=1){
 	return(n * factorial(n-1));
     }
     else return 1;
